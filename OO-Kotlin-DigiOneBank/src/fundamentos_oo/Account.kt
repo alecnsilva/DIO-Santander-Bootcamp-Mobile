8f0edcd8b776e90420f789077b092ee2d56fb945
Alec N. Silva <alec.ns@pm.me>
2021-08-03 21:08:18 -0300

    - Class Member Function, Properties

---
+++ OO-Kotlin-DigiOneBank/src/fundamentos_oo/Account.kt
@@ -0,0 +1,14 @@
+package fundamentos_oo
+
+import java.math.BigDecimal
+
+class Account(
+    val agency: String,
+    val number: String,
+    val balance: BigDecimal
+) {
+    fun deposit(value: BigDecimal) {
+    }
+    fun withdraw(value: BigDecimal) {
+    }
+}
