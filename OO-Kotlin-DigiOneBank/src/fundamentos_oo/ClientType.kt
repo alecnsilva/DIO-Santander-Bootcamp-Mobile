bfdae8053fd71eb0294c4e61f411140af95d8020
Alec N. Silva <alec.ns@pm.me>
2021-08-04 17:07:24 -0300

    - Understanding Enum in Practice - Part 2

--- OO-Kotlin-DigiOneBank/src/fundamentos_oo/ClientType.kt
+++ OO-Kotlin-DigiOneBank/src/fundamentos_oo/ClientType.kt
@@ -1,6 +1,6 @@
 package fundamentos_oo
 
-enum class ClientType(descricao: String) {
+enum class ClientType(val descricao: String) {
     Pf("Pessoa Física"),
     Pj("Pessoa Jurídica"),
 }
--- OO-Kotlin-DigiOneBank/src/fundamentos_oo/tests/TestClientTypeEnum.kt
+++ OO-Kotlin-DigiOneBank/src/fundamentos_oo/tests/TestClientTypeEnum.kt
@@ -4,10 +4,16 @@ import fundamentos_oo.ClientType
 
 fun main() {
     ClientType.values().forEach { elements ->
-        println("${elements.name} - $elements\n")
+        println("${elements.name} - ${elements.descricao}\n")
     }
 
     ClientType.values().forEach {
-        println("${it.name} - $it\n")
+        println("${it.name} - ${it.descricao}\n")
     }
+
+    val Pf = ClientType.Pf
+    println(">> ${Pf.name} - ${Pf.descricao}")
+
+    val Pj = ClientType.Pj
+    println(">> ${Pj.name} - ${Pj.descricao}")
 }
